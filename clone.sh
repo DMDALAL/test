3a0f4b3e37c63946571f9c24a582e727bb1c466e
Dylan Dalal <103948543+DMDALAL@users.noreply.github.com>
2022-04-21 15:39:24 -0400

    Add files via upload

---
+++ clone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone "https://${1}@github.com/marzvfx/MARZ_DL.git"
+git clone "https://${1}@github.com/marzvfx/marz_data.git"
+git clone "https://${1}@github.com/marzvfx/marzml.git"
+git clone "https://${1}@github.com/marzvfx/Vanity-StyleGAN.git"
+git clone "https://${1}@github.com/marzvfx/wandb_utils.git"
+git clone "https://${1}@github.com/marzvfx/MARZ_MicroServices.git"
+git clone "https://${1}@github.com/marzvfx/github_automation.git"
+git clone "https://${1}@github.com/marzvfx/Wav2Lip.git"
+git clone "https://${1}@github.com/marzvfx/DeepFaceLabDocker.git"
+git clone "https://${1}@github.com/marzvfx/ffmpeg_frame_extractor.git"
